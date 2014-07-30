d61a5802ae068338a5d1e6b1a845541667a48ded
NimitzDEV <nimitzdevteam@gmail.com>
2014-07-30 23:16:21 +0800

    取消路径选择，默认将使用文档路径

--- frmMain.vb
+++ frmMain.vb
@@ -1,19 +1,18 @@
-﻿Public Class frmMain
+﻿Imports Microsoft.VisualBasic.FileIO.FileSystem
+Public Class frmMain
     Dim getStr As String
-
+    Dim binFolder As String
     Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
         writeBINConfig()
     End Sub
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        binFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Proxy Manager\"
+        If DirectoryExists(binFolder) = False Then MkDir(binFolder)
+        binPath = binFolder & "proxylist.bin"
         getStr = funcGetRegeditValue("ProxyServer")
         txtIPAddress.Text = Split(getStr, ":")(0)
         txtPort.Text = Split(getStr, ":")(1)
         Me.Text = Application.ProductName & " - " & Application.ProductVersion
-        If My.Settings.listPath = "NONE" Then
-            MsgBox("需要设置代理列表保存路径，请选择一个吧")
-            xmlPathSet()
-        End If
-        binPath = My.Settings.listPath & "\proxylist.bin"
         readBINConfig()
     End Sub
 
@@ -29,18 +28,6 @@
     Private Sub btnProxyDisable_Click(sender As Object, e As EventArgs) Handles btnProxyDisable.Click
         cancelProxy()
         MsgBox("代理已关闭")
-    End Sub
-
-    Private Sub xmlPathSet()
-        fbd.ShowNewFolderButton = True
-        fbd.Description = "请选择一个存放列表保存的地方"
-        fbd.ShowDialog()
-        If fbd.SelectedPath = "" Then
-            MsgBox("没有进行选择")
-            Exit Sub
-        End If
-        My.Settings.listPath = fbd.SelectedPath
-        My.Settings.Save()
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
--- mdConfig.vb
+++ mdConfig.vb
@@ -4,20 +4,20 @@ Module mdConfig
     Public binPath As String
     Public Sub writeBINConfig()
         Dim writeString As String = ""
-        Dim FS As New System.IO.FileStream(binPath, FileMode.Create)
-        Dim Bw As New System.IO.BinaryWriter(FS, System.Text.Encoding.Unicode)
         Try
             With frmMain
                 If .ListView1.Items.Count = 0 Then
                     If FileExists(binPath) = True Then DeleteFile(binPath)
                     Exit Sub
                 End If
+                Dim FS As New System.IO.FileStream(binPath, FileMode.Create)
+                Dim Bw As New System.IO.BinaryWriter(FS, System.Text.Encoding.Unicode)
                 For i = 0 To .ListView1.Items.Count - 1
                     Bw.Write(.ListView1.Items(i).Tag)
                 Next
+                Bw.Close()
+                FS.Close()
             End With
-            Bw.Close()
-            FS.Close()
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
